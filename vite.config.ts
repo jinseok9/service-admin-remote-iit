70879d766df3af4559f848a8d77f1f3aaa3d3683
jinseok <jinseok@jobis.co>
2024-12-04 06:05:41 +0900

    chore: 리모트 환경 셋팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: "iit",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./IIT": "./src/App.tsx",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
 });
